c78ba9d3c88d3fc095ef3ee7218be0ec826bbf05
Brendan Ralston <ralston.brendan@me.com>
2019-05-25 12:52:36 -0600

    Add header file for Candle class

---
+++ Candle.h
@@ -0,0 +1,22 @@
+/*
+ * Candle object header
+ * Brendan Ralston, 5/25/19
+ */
+
+ #ifndef Candle_h
+ #define Candle_h
+
+ #include "Arduino.h"
+
+ class Candle
+ {
+  public:
+    Candle(byte pin);
+    void on();
+    void off();
+    void toggle();
+    bool isLit();
+  private:
+    byte _pin;
+    bool _state;
+ }
