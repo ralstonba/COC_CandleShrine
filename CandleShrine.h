cbdac555700bbbacac4c9f7a459061d4a5974978
Brendan Ralston <ralston.brendan@me.com>
2019-05-25 13:25:07 -0600

    Shrine header

---
+++ CandleShrine.h
@@ -0,0 +1,19 @@
+/*
+ * CandleShrine object header
+ * Brendan Ralston, 5/25/19
+ */
+
+ #ifndef CandleShrine_h
+ #define CandleShrine_h
+
+ #include "Arduino.h"
+ #include "Candle.h"
+
+ class CandleShrine
+ {
+  public:
+    // This is gross imo
+    CandleShrine(byte candle1Pin, byte candle2Pin, byte candle3Pin, byte candle4Pin, byte candle5Pin, byte candle6Pin, byte candle7Pin, byte candle8Pin);
+  private:
+    Candle candles[8];
+ }
